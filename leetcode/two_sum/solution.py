c5486283674b68099968981e4d3e364ffb0faa44
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2025-09-13 00:03:59 +0700

    test: add more test cases

--- leetcode/two_sum/solution.py
+++ leetcode/two_sum/solution.py
@@ -1,7 +1,12 @@
 class Solution:
 
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(n)
     def two_sum(self, nums: list[int], target: int) -> list[int]:
-        # TODO: Implement two_sum
+        seen: dict[int, int] = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
         return []
