a781eb92c61f1b1a8487765b964c6addf16b8497
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2025-10-04 11:37:54 +0700

    fix: fix two sum test cases

--- leetcode/two_sum/solution.py
+++ leetcode/two_sum/solution.py
@@ -4,9 +4,16 @@ class Solution:
     # Space: O(n)
     def two_sum(self, nums: list[int], target: int) -> list[int]:
         seen: dict[int, int] = {}
+        answers: list[list[int]] = []
+
         for i, num in enumerate(nums):
             complement = target - num
             if complement in seen:
-                return [seen[complement], i]
+                answer = [seen[complement], i]
+                answers.append(answer)
             seen[num] = i
-        return []
+
+        if len(answers) > 1:
+            raise ValueError(f"Found {len(answers)} answers in the solution: {answers}")
+
+        return answers[0] if answers else []
